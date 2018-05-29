0b61e5595a0d9cc9e74629730fc4989fdf518341
Azavadin <scharantejams@gmail.com>
2018-05-29 11:48:50 -0500

    Completed Project

---
+++ src/main/java/rest/services/springboot/studentservicesREST/StudentServicesRestApplication.java
@@ -0,0 +1,12 @@
+package rest.services.springboot.studentservicesREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesRestApplication.class, args);
+	}
+}
---
+++ src/test/java/rest/services/springboot/studentservicesREST/StudentServicesRestApplicationTests.java
@@ -0,0 +1,16 @@
+package rest.services.springboot.studentservicesREST;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentServicesRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
